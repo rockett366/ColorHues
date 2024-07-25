2d58bf4db6716275c949f826e7fe2d294d62e899
rockett366 <91300613+rockett366@users.noreply.github.com>
2024-07-24 17:48:11 -0700

    start of the Color Hue
    got the basic of switch color

---
+++ colorhues/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import Left from "../sides/Left";
+import Right from "../sides/Right";
+
+function Home() {
+  return (
+    <div className="home">
+      <Left />
+      <Right />
+    </div>
+  );
+}
+
+export default Home;
---
+++ colorhues/src/components/sides/Right.js
@@ -0,0 +1,9 @@
+function Right() {
+  return (
+    <div className="mainComponent">
+      <div></div>
+    </div>
+  );
+}
+
+export default Right;

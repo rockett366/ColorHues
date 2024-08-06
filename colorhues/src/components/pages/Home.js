c77f8e5d824b34f4bd40269545d6499e1752d79d
rockett366 <91300613+rockett366@users.noreply.github.com>
2024-08-05 21:50:11 -0700

    change of colors when click different buttons on left pannel

--- colorhues/src/components/pages/Home.js
+++ colorhues/src/components/pages/Home.js
@@ -1,10 +1,33 @@
-import Left from "../sides/Left";
 import Right from "../sides/Right";
+import useLocalStorage from "use-local-storage";
 
 function Home() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  const switchThemeLight = () => {
+    setTheme("light");
+  };
+  const switchThemeDark = () => {
+    setTheme("dark");
+  };
+  const switchThemePink = () => {
+    setTheme("pink");
+  };
+  const switchThemeNatural = () => {
+    setTheme("natural");
+  };
+
   return (
-    <div className="home">
-      <Left />
+    <div className="home" data-theme={theme}>
+      <div className="leftControl">
+        <button onClick={switchThemeLight}>light </button>
+        <button onClick={switchThemeDark}>Dark</button>
+        <button onClick={switchThemePink}>pink</button>
+        <button onClick={switchThemeNatural}>nude</button>
+      </div>
       <Right />
     </div>
   );
--- colorhues/src/components/sides/Right.js
+++ colorhues/src/components/sides/Right.js
@@ -1,7 +1,42 @@
+import { LuFish } from "react-icons/lu";
+
 function Right() {
+  const assignRef = (element) => {
+    this.container = element;
+  };
+  assignRef(this);
+  const getStyle = () => {
+    const styles = this.container.style;
+    console.log(styles);
+    // for getting computed styles
+    const computed = window
+      .getComputedStyle(this.container)
+      .getPropertyValue("background-color"); // border-radius can be replaced with any other style attributes;
+    console.log(computed);
+  };
+
   return (
     <div className="mainComponent">
-      <div></div>
+      <div className="title">
+        <h1>Color Hue</h1>
+      </div>
+      <div className="firstContainter">
+        <div className="logoContainer">
+          <LuFish className="logo" />
+        </div>
+        <div className="firstContainerWords">
+          <h2>A Colorful Pallet</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          </p>
+        </div>
+      </div>
+      <div className="closureComponent">
+        <h2>Color Pallet</h2>
+        <p>Background: #fff {getStyle()}</p>
+      </div>
     </div>
   );
 }

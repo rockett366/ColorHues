2baa7ee18bee73e4c61363a9811bbcf0a290f4ba
rockett366 <91300613+rockett366@users.noreply.github.com>
2024-08-11 16:29:29 -0700

    testing how to pull hex number

--- colorhues/src/components/pages/Home.js
+++ colorhues/src/components/pages/Home.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import Right from "../sides/Right";
 import useLocalStorage from "use-local-storage";
 
@@ -20,8 +21,28 @@ function Home() {
     setTheme("natural");
   };
 
+  const elementRef = useRef(null);
+  useEffect(() => {
+    const element = elementRef.current;
+    if (element) {
+      const computedStyle = window.getComputedStyle(element);
+      const cssProperties = Array.from(computedStyle).reduce(
+        (acc, property) => {
+          acc[property] = computedStyle.getPropertyValue(property);
+          return acc;
+        },
+        {}
+      );
+
+      const cssPropertiesString = JSON.stringify(cssProperties, null, 2);
+      document.getElementById("css-output").innerText = cssPropertiesString;
+
+      console.log(cssProperties);
+    }
+  }, []);
+
   return (
-    <div className="home" data-theme={theme}>
+    <div className="home" data-theme={theme} id="home">
       <div className="leftControl">
         <button onClick={switchThemeLight}>light </button>
         <button onClick={switchThemeDark}>Dark</button>
--- colorhues/src/components/sides/Right.js
+++ colorhues/src/components/sides/Right.js
@@ -1,19 +1,36 @@
 import { LuFish } from "react-icons/lu";
+// import { MainBackgound } from "../../App.css";
 
 function Right() {
-  const assignRef = (element) => {
-    this.container = element;
-  };
-  assignRef(this);
-  const getStyle = () => {
-    const styles = this.container.style;
-    console.log(styles);
-    // for getting computed styles
-    const computed = window
-      .getComputedStyle(this.container)
-      .getPropertyValue("background-color"); // border-radius can be replaced with any other style attributes;
-    console.log(computed);
-  };
+  // const assignRef = (element) => {
+  //   this.container = element;
+  // };
+  // assignRef(this);
+  // const getStyle = () => {
+  //   const styles = this.container.style;
+  //   console.log(styles);
+  //   // for getting computed styles
+  //   const computed = window
+  //     .getComputedStyle(this.container)
+  //     .getPropertyValue("background-color"); // border-radius can be replaced with any other style attributes;
+  //   console.log(computed);
+  // };
+
+  // const myObj = window.getComputedStyle(document.getElementById("home"));
+  // let myelement = myObj.getPropertyValue("background-color");
+
+  // const element = await Right.waitForSelector("div");
+  // const color = await element.evaluate((el) => {
+  //   return window.getComputedStyle(el).getPropertyValue("background-color");
+  // });
+  // console.log(color);
+
+  // const colorDisplay = () => {
+  //   let element3 = document.getElementById("exampleElement");
+  //   let backgroundColor3 = element3.style.backgroundColor;
+  //   document.getElementById("colorDisplay").innerText +=
+  //     "\nColor: " + backgroundColor3;
+  // };
 
   return (
     <div className="mainComponent">
@@ -33,10 +50,11 @@ function Right() {
           </p>
         </div>
       </div>
-      <div className="closureComponent">
+      <div className="closureComponent" id="exampleElement">
         <h2>Color Pallet</h2>
-        <p>Background: #fff {getStyle()}</p>
+        <p>Background: #fff </p>
       </div>
+      <p id="colorDisplay"></p>
     </div>
   );
 }
